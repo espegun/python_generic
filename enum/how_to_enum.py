9e8db9f0c6d840e9a9b47ee4677ec9b24bcea9dc
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-10-28 14:45:56 +0100

    Add enumeration

---
+++ enum/how_to_enum.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+"""Set symbolic names bound to unique, constant values."""
+
+# Define
+class Colors(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+print(Colors.RED.name)  # RED
+print(Colors.RED.value)  # 1
+try:
+    print(Colors.YELLOW)
+except AttributeError:
+    print("Could not find Colors.YELLOW.")
+
+# Iterate though
+for col in Colors:
+    print(col.name, col.value)
+
+# Comparison (these are all True)
+print(Colors.RED is Colors.RED)
+print(Colors.RED is not Colors.GREEN)
+print(Colors.RED == Colors.RED)
+print(Colors.RED != Colors.GREEN)
+
+
+print("That was fun.")
+
