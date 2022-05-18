caa1a8657ecc01e46f6c3f143576846a9b61ec8c
Espen Gunnarsen <espen.gunnarsen@gmail.com>
2022-05-18 08:25:11 +0200

    sub

--- regex_testing/regex.py
+++ regex_testing/regex.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 PATTERN = r"jalla: (\d+) .*? strengen: (\".*?\")"
 
@@ -25,5 +26,14 @@ for line in lines:
         print(f"match.group(0), i.e. the full expression: {b.group(0)}")
         print(f"match.group(1), i.e. captured group 1: {b.group(1)}")
         print(f"match.group(2), i.e. captured group 2: {b.group(2)}")
+
+### Make substitutiion string 
+# \1 and \2 refers to capture groups
+captured_info = []
+for line in lines:
+    if re.match(PATTERN, line):
+        json_str = re.sub(PATTERN, r'{"jalla": \1, "strengen": \2}', line)  # JSON str, not dict
+        captured_info.append(json.loads(json_str))  # Append, as a dict
+print(captured_info)
 
 print("That was fun.")
