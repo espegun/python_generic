5b1db1f95d4677a1aca73682a300ac7ae95db48b
Espen Gunnarsen <espen.gunnarsen@gmail.com>
2022-09-12 13:57:56 +0200

    Copy cookbook

--- how_to_tkinter/gui_thread.py
+++ how_to_tkinter/gui_thread.py
@@ -1,6 +1,30 @@
+# import tkinter as tk
+# root = tk.Tk() 
+
 import tkinter as tk
+import time
+import queue
 import the_threads
 import concurrent.futures
+
+class ThreadedClient:
+
+    def __init__(self, root):
+
+        self.root = root
+        self.queue = queue.Queue()
+        self.gui = Guipart(self.root, self.queue)
+
+        self.running = True
+        self.thread1 = start_threads(None)
+        
+        self.periodic_call()
+
+    def periodic_call(self):
+
+        self.gui.process_incoming()
+        self.root.after(200, self.periodic_call())
+
 
 def thread_function(name):
 
@@ -16,22 +40,34 @@ def start_threads(event):
         
     print("All threads are finished!")
 
+class Guipart:
+
+    def __init__(self, root, queue, endCommand=None):
+        
+        self.queue = queue
+
+        btn_only = tk.Button(root, text="Click me")
+        lbl_1 = tk.Label(root, text="1")
+        lbl_2 = tk.Label(root, text="1")
+        lbl_3 = tk.Label(root, text="1")
+
+        btn_only.grid(column=0, row=0, rowspan=3, sticky="ns", padx=5, pady=5)
+        lbl_1.grid(column=1, row=0, stick="nsew", padx=1, pady=1)
+        lbl_2.grid(column=1, row=1, stick="nsew", padx=1, pady=1)
+        lbl_3.grid(column=1, row=2, stick="nsew", padx=1, pady=1)
+
+        btn_only.bind("<Button-1>", start_threads)
+
+    def process_incoming(self):
+        "If there's anything in the queue, handle it."
+
+        while self.queue.qsize() > 0:
+            msg = self.queue.get(0)
+            print(msg)  # Or something more clever
 
 root = tk.Tk()
-
-btn_only = tk.Button(root, text="Click me")
-lbl_1 = tk.Label(root, text="1")
-lbl_2 = tk.Label(root, text="1")
-lbl_3 = tk.Label(root, text="1")
-
-btn_only.grid(column=0, row=0, rowspan=3, sticky="ns", padx=5, pady=5)
-lbl_1.grid(column=1, row=0, stick="nsew", padx=1, pady=1)
-lbl_2.grid(column=1, row=1, stick="nsew", padx=1, pady=1)
-lbl_3.grid(column=1, row=2, stick="nsew", padx=1, pady=1)
-
-btn_only.bind("<Button-1>", start_threads)
-
-tk.mainloop()
+client = ThreadedClient(root)
+root.mainloop()
 
 ################ NOW - TRY THIS ######
 # https://www.oreilly.com/library/view/python-cookbook/0596001673/ch09s07.html
