9a7b74912b5205c1d1fdfafa3384002d7d0294ee
Espen Gunnarsen <espen.gunnarsen@huddly.com>
2022-10-13 15:07:23 +0200

    Threading with results

--- how_to_threading/basic_threading.py
+++ how_to_threading/basic_threading.py
@@ -1,5 +1,19 @@
 import threading
 import time
+import random
+
+
+class CanStoreResults:
+
+    def __init__(self):
+
+        self.result = None
+
+    def func_with_result(self):
+
+        time.sleep(random.random())
+        self.result = "".join(random.choices(list("ABCDEF"), k=3)) 
+        return self.result  # <-- This won't go anywhere 
 
 def threading_function(name: str, sleep_time: float):
 
@@ -14,6 +28,8 @@ if __name__ == "__main__":
     th1.start()
     th1.join()  # Wait for it to finish
 
+    print("================")
+
     print("Several Threads")
     th2 = threading.Thread(target=threading_function, args=("B", 5))
     th3 = threading.Thread(target=threading_function, args=("C", 2))
@@ -22,5 +38,20 @@ if __name__ == "__main__":
     for th in [th2, th3]:
         th.join()
     print("All the threads have finished.")
+
+    print("================")
+
+    print("Several threads with result, need to save the result to something, not return it.")
+   
+    CSR = [CanStoreResults(), CanStoreResults()]
+    th4 = threading.Thread(target=CSR[0].func_with_result)
+    th5 = threading.Thread(target=CSR[1].func_with_result)
+    for th in [th4, th5]:
+        print(th.start())
+    for th in [th4, th5]:
+        print(th.join())
+
+    for csr in CSR:
+        print(csr.result)
 
     print("That was fun.")
