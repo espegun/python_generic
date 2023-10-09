e2e612ace78949437f9ebc1c643bf155b9dd0e0c
Espen Gunnarsen <espen.gunnarsen@huddly.com>
2023-10-09 08:40:02 +0200

    Update with timestamp

--- time_and_dates/basics_datetime_date_timedelta.py
+++ time_and_dates/basics_datetime_date_timedelta.py
@@ -1,15 +1,25 @@
 import datetime
+import time
 
+print("datetime.date")
 date1 = datetime.date(2021, 5, 12)  # Type datetime.date, printed as "2021-05-12"
 date2 = date1 + datetime.timedelta(days=5)  # Type datetime.date, printed as "2021-05-17"
 date3 = datetime.date.today()  # Type datetime.date 
 for date in [date1, date2, date3]:
     print(type(date), date)
 
+print("datetime.datetime")
 dt1 = datetime.datetime(2021, 5, 21, 12, 0, 0)  # Type datetime.datetime, printed as "2021-05-21 12:00:00"
 dt2 = dt1 + datetime.timedelta(days=1, seconds=30, minutes=10, hours=1)  # Type datetime.datetime, printed as "2021-05-22 13:10:30
 dt3 = datetime.datetime.now()  # Type datetime.datetime, printed as "2023-10-06 14:41:07.285696"
 for dt in [dt1, dt2, dt3]:
     print(type(dt), dt)
+
+print("\nTo and from timestamps (can also be used on datetime.date)")
+dt4 = datetime.datetime.now()
+time.sleep(1)
+dt5 = datetime.datetime.now()
+print(dt4, dt4.timestamp(), datetime.datetime.fromtimestamp(dt4.timestamp()))
+print(dt5, dt5.timestamp(), datetime.datetime.fromtimestamp(int(dt5.timestamp())))
 
 print("That was fun!")
